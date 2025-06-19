920b7dd34dd6bf929e8d7f3b8c784c96daaa8c98
Viacheslav Titov <viacheslavtitov90@gmail.com>
2025-06-19 13:45:20 +0300

    fixed swagger for sign in

--- api/controller/auth/auth_controller.go
+++ api/controller/auth/auth_controller.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	middleware "easy-dictionary-server/api/middleware"
-	"easy-dictionary-server/domain"
+	domainAuth "easy-dictionary-server/domain"
 	internalenv "easy-dictionary-server/internalenv"
 	validatorutil "easy-dictionary-server/internalenv/validator"
 
@@ -14,7 +14,7 @@ import (
 )
 
 type AuthController struct {
-	AuthUseCase domain.AuthUseCase
+	AuthUseCase domainAuth.AuthUseCase
 	Env         *internalenv.Env
 }
 
@@ -24,6 +24,7 @@ type AuthController struct {
 // @Tags         auth
 // @Accept       json
 // @Produce      json
+// @Param   input body domainAuth.AuthRequest true "User data"
 // @Success      200  {string}  Barear Access Token
 // @Failure      400  {object}  domain.ErrorResponse
 // @Failure      404  {object}  domain.ErrorResponse
@@ -31,7 +32,7 @@ type AuthController struct {
 // @Router       /api/signin [post]
 func (authController *AuthController) Login(c *gin.Context) {
 	zap.S().Info("POST Login")
-	var request domain.AuthRequest
+	var request domainAuth.AuthRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
 		zap.S().Error(err)
 		validationErrors := validatorutil.FormatValidationError(err)
@@ -41,17 +42,17 @@ func (authController *AuthController) Login(c *gin.Context) {
 	if request.Provider == "email" {
 		if request.Password == "" {
 			zap.S().Error("Password is empty")
-			c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: "Password is required"})
+			c.JSON(http.StatusBadRequest, domainAuth.ErrorResponse{Message: "Password is required"})
 			return
 		}
 		if request.Email == "" {
 			zap.S().Error("Email is empty")
-			c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: "Email is required"})
+			c.JSON(http.StatusBadRequest, domainAuth.ErrorResponse{Message: "Email is required"})
 			return
 		}
 		if !middleware.ValidatePassword(request.Password) {
 			zap.S().Error("Password is not valid")
-			c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: "Password validation failed"})
+			c.JSON(http.StatusBadRequest, domainAuth.ErrorResponse{Message: "Password validation failed"})
 			return
 		}
 	}
@@ -60,18 +61,18 @@ func (authController *AuthController) Login(c *gin.Context) {
 	if err != nil || (user == nil || userId == nil) {
 		zap.S().Errorf("User with %s %s not found", request.Provider, request.Email)
 		zap.S().Error(err)
-		c.JSON(http.StatusForbidden, domain.ErrorResponse{Message: "User not found with the " + request.Email + " email"})
+		c.JSON(http.StatusForbidden, domainAuth.ErrorResponse{Message: "User not found with the " + request.Email + " email"})
 		return
 	} else {
 		zap.S().Debugf("User found %s %s %s", user.FirstName, user.LastName, user.UUID)
 		if request.Provider == "email" {
 			emailProvider := user.FindEmailProvider()
 			if emailProvider == nil {
-				c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: "User is not registered by " + request.Email + " email"})
+				c.JSON(http.StatusBadRequest, domainAuth.ErrorResponse{Message: "User is not registered by " + request.Email + " email"})
 				return
 			}
 			if !middleware.ComparePassword(request.Password, emailProvider.HashedPassword) {
-				c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: "Password validation failed"})
+				c.JSON(http.StatusBadRequest, domainAuth.ErrorResponse{Message: "Password validation failed"})
 				return
 			} else {
 				zap.S().Debug("User validated by password")
@@ -85,11 +86,11 @@ func (authController *AuthController) Login(c *gin.Context) {
 		if err != nil {
 			zap.S().Errorf("Failed to create access token for user %s", user.UUID)
 			zap.S().Error(err)
-			c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+			c.JSON(http.StatusInternalServerError, domainAuth.ErrorResponse{Message: err.Error()})
 			return
 		}
 
-		authResponse := domain.AuthResponse{
+		authResponse := domainAuth.AuthResponse{
 			AccessToken: accessToken,
 		}
 		c.JSON(http.StatusOK, authResponse)
