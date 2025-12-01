8e5a1cc0da049d1dbec5ab0ce2ea9251ade6f548
Viacheslav Titov <viacheslavtitov90@gmail.com>
2025-12-01 18:19:19 +0200

    fixed query to get words from db

--- db/word/word_query.go
+++ db/word/word_query.go
@@ -13,36 +13,18 @@ package db
 // - $9: query by original field (text, nullable/empty)
 func getAllWordsByDictionaryQuery() string {
 	return `
-WITH page AS (
-  SELECT id
-  FROM word
-  WHERE dictionary_id = $1
-    AND id > $2               -- $2 = lastId (0 for first page)
-    AND ($4::timestamp IS NULL OR created_at >= $4::timestamp) -- $4 created_from
-    AND ($5::timestamp IS NULL OR created_at <= $5::timestamp) -- $5 created_to
-    AND (
-      $6::text[] IS NULL
-      OR type = ANY($6::text[])          -- $6 word_types
-    )
-    AND (
-      $9::text IS NULL
-      OR original ILIKE '%' || $9::text || '%' -- $9
-    )
-  ORDER BY id
-  LIMIT $3                    -- $3 = pageSize (+1, if you want to know has_more)
-)
 SELECT
   w.id            AS word_id,
   w.dictionary_id AS word_dictionary_id,
   w.original      AS word_original,
   w.phonetic      AS word_phonetic,
   w.type          AS word_type,
-  w.created_at AS word_created_at,
+  w.created_at    AS word_created_at,
 
   t.id            AS translation_id,
   t.description   AS translation_description,
   t.translate     AS translation_text,
-  t.created_at AS translation_created_at,
+  t.created_at    AS translation_created_at,
 
   tc.id           AS category_id,
   tc.name         AS category_name,
@@ -50,7 +32,6 @@ SELECT
   wt.id           AS tag_id,
   wt.name         AS tag_name
 FROM word w
-JOIN page p           ON p.id = w.id
 LEFT JOIN translation t
        ON t.word_id = w.id
 LEFT JOIN translation_category tc
@@ -62,28 +43,16 @@ LEFT JOIN word_tag wt
        ON wt.id = wtw.word_tag_id
       AND wt.dictionary_id = w.dictionary_id
 WHERE
-  (
-    -- 1) if all filters are null
-    $6::text[] IS NULL
-    AND $7::int[] IS NULL
-    AND $8::int[] IS NULL
-  )
-  OR (
-    -- 2) find in word_types
-    $6::text[] IS NOT NULL
-    AND w.type = ANY($6::text[])
-  )
-  OR (
-    -- 3) find in category_ids
-    $7::int[] IS NOT NULL
-    AND tc.id = ANY($7::int[])
-  )
-  OR (
-    -- 4) find in tag_ids
-    $8::int[] IS NOT NULL
-    AND wt.id = ANY($8::int[])
-  )
-ORDER BY w.id, t.id, wt.id;`
+  w.dictionary_id = $1
+  AND w.id > $2              -- lastId
+  AND ($4::timestamp IS NULL OR w.created_at >= $4::timestamp)
+  AND ($5::timestamp IS NULL OR w.created_at <= $5::timestamp)
+  AND ($6::text[] IS NULL OR w.type = ANY($6::text[]))
+  AND ($7::int[] IS NULL OR tc.id = ANY($7::int[]))
+  AND ($8::int[] IS NULL OR wt.id = ANY($8::int[]))
+  AND ($9::text IS NULL OR w.original ILIKE '%' || $9::text || '%')
+ORDER BY w.id, t.id, wt.id
+LIMIT $3;`
 }
 
 // CreateWordAndReturnIdQuery get query to create word
